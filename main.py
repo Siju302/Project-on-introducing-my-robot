3f5f69e1fabd4684c3e601da83c93efade3ac5ef
Siju302 <sijuadeoyelami10@gmail.com>
2025-04-12 12:23:24 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+class robot:
+    name = "Jada"
+
+    def introduction(self):
+        print("Hi I am a robot")
+
+    def details(self):
+        print("My name is", self.name)
+
+ob = robot()
+ob.introduction()
+ob.details()
